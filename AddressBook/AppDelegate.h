2e0e02f291b1e9679e8c12a4074bf118972199a7
Saulo Arruda <sauloarruda@gmail.com>
2012-07-16 19:35:15 -0400

    Initial Commit

---
+++ AddressBook/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  AddressBook
+//
+//  Created by Saulo Arruda Coelho on 7/16/12.
+//  Copyright (c) 2012 Jera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ AddressBook/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AddressBook
+//
+//  Created by Saulo Arruda Coelho on 7/16/12.
+//  Copyright (c) 2012 Jera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
